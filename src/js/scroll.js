9b7bc5cd53792963735b3f052e2564b3c3a199ef
zhangqf <15893652937@163.com>
2018-08-28 16:05:09 +0800

    Please enter the commit message for your changes. Lines starting   var target = this;     setTimeout(function(){             target.scrollIntoViewIfNeeded(); 	            console.log('scrollIntoViewIfNeeded'); 		          },400); 			  });$$('input[type="text"],textarea').on('click', 			  function () { 			    var target = this; 			      setTimeout(function(){ 			              target.scrollIntoViewIfNeeded(); 				              console.log('scrollIntoViewIfNeeded'); 					           area').on('click', function () {   var target = this;   setTimeout(function(){         target.scrollIntoViewIfNeeded();         console.log('scrollIntoViewIfNeeded');       },400); });tml

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,1 +1,10 @@
-console.log(dpr)
+$(".ui-list").on("click", "span", function() {
+    var index = $(this).index() - 1;
+    $(this).addClass("active").siblings().removeClass("active")
+    if (index < 0) {
+        $(".ui-menu-content .ui-create").show()
+    } else {
+        $(".ui-menu-content .ui-create").eq(index).show().siblings().hide()
+    }
+
+})
